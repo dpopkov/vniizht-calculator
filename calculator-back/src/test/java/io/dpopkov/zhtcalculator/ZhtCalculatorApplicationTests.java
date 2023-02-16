fdb4b1459501a5712795aa255f25488b876b3eb2
Denis Popkov <pkvdenis@gmail.com>
2023-02-16 12:32:18 +0300

    back: add empty Spring Boot project.

---
+++ calculator-back/src/test/java/io/dpopkov/zhtcalculator/ZhtCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dpopkov.zhtcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhtCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
