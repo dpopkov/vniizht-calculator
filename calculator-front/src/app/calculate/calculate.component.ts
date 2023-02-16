7cc161a10fcf33cf1d85f4b077c0e46e01cf68a0
Denis Popkov <pkvdenis@gmail.com>
2023-02-17 02:28:18 +0300

    front: display error message.

--- calculator-front/src/app/calculate/calculate.component.ts
+++ calculator-front/src/app/calculate/calculate.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {CalculateRequest} from "../model/calculate-request";
 import {DataService} from "../data.service";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-calculate',
@@ -18,13 +19,12 @@ export class CalculateComponent implements OnInit {
   }
 
   onSubmit(): void {
-    console.log('Sending expression: ', this.formCalculate.expression);
     this.dataService.calculate(this.formCalculate).subscribe(
       result => {
-        console.log('Received result ', result);
         this.message = 'Результат = ' + result;
       },
-      error => {
+      (error: HttpErrorResponse) => {
+        this.message = error.error?.message;
         console.log('Something went wrong: ', error);
       }
     )
